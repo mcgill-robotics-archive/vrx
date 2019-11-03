affe9bb4648c319d9f5c3a974229414853f778d1
Yasasa Abeysirigoonawardena <yasasa.abey@gmail.com>
2019-11-03 15:27:31 -0500

    Add new holonomic controller

--- catkin_ws/src/control/setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"""MR VRX Control - Python Setup"""
-
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(packages=['control'], package_dir={'': 'src'})
-
-setup(**d)
